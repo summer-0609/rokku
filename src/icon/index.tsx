0cdc31c8a7936036ffe1ab43c0050609a2b41d38
zhuding.zhang <zhuding.zhang@ly.com>
2021-01-27 16:07:20 +0800

    fix: icon touchstart无法preventDefault问题

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import { addUnit, createNamespace } from '../utils';
 import { IconPropsType } from './PropsType';
@@ -21,10 +21,18 @@ const [bem] = createNamespace('icon');
 const Icon: React.FC<IconProps> = (props) => {
   const { tag = 'i', name, className, onClick, onTouchStart } = props;
   const imageIcon = isImage(name);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // 解决touchstart无法preventDefault的问题
+    // touchstart事件passive默认开启
+    ref.current?.addEventListener('touchstart', onTouchStart, { passive: false });
+  }, []);
 
   return React.createElement(
     tag,
     {
+      ref,
       className: classnames(
         className,
         props.classPrefix,
@@ -35,7 +43,6 @@ const Icon: React.FC<IconProps> = (props) => {
         fontSize: addUnit(props.size),
       },
       onClick,
-      onTouchStart
     },
     props.children,
     imageIcon && <img className={classnames(bem('image'))} src={name} alt={name} />,
